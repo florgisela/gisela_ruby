f352db608070624cc446225484f8950f974581de
gisela <pahuara.chinita@gmail.com>
2016-04-14 11:23:28 -0500

    segundo archivo:ejercicio02.rb

---
+++ ejercicio02.rb
@@ -0,0 +1,11 @@
+refrescos=["limonada","chicha morada","chicha de jura","maracuya"]
+
+puts "mi refrescos favorito es :"+refrescos[1]
+
+puts "======"
+
+refrescos[0..3].each {|re|
+
+puts "mi refresco favorito es :"+re
+}
+gets()
